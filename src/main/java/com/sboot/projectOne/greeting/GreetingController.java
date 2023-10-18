54ad5a24b4c0f99efa4927c7785ecaa6903c959e
PradeepVPK <pradeepkumar15v06@gmail.com>
2023-10-18 22:20:53 +0530

    First Commit

---
+++ src/main/java/com/sboot/projectOne/greeting/GreetingController.java
@@ -0,0 +1,27 @@
+package com.sboot.projectOne.greeting;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    @ResponseBody
+    public String getGreet(){
+        System.out.println("hello");
+        return "hello user in get method";
+    }
+
+    @PostMapping("/greet")
+    @ResponseBody
+    public String postGreet(){
+        return "hello user in post method";
+    }
+}
